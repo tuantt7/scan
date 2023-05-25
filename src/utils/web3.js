c0eafdf9d254fb654491d0221877d7484b6db857
tuantt7 <tranthanhtuan62@gmail.com>
2023-05-25 13:44:31 +0700

    Change

--- src/utils/web3.js
+++ src/utils/web3.js
@@ -5,9 +5,11 @@ const mainnetNetwork = import.meta.env.VITE_MAINNET_NETWORK
 const apiKey = import.meta.env.VITE_INFURA_API_KEY
 // const privateKey = import.meta.env.VITE_SIGNER_PRIVATE_KEY
 
+if (!localStorage.getItem('net')) {
+  localStorage.setItem('net', 'sepolia')
+}
 const net = localStorage.getItem('net')
-const network =  net === sepoliaNetwork ? sepoliaNetwork : mainnetNetwork
-
+const network = net === sepoliaNetwork ? sepoliaNetwork : mainnetNetwork
 
 const web3 = new Web3(new Web3.providers.HttpProvider(`https://${network}.infura.io/v3/${apiKey}`))
 
